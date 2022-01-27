3c982779296673e2837b31e937321e99c0f7dd71
Alex Palx <alex.teanow@gmail.com>
2022-01-27 22:36:23 +0300

    feat: add theme change

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,3 +1,11 @@
+let theme = (document.body.classList.contains('light-theme')) ? 1 : 0;
+const themeButton = document.querySelector('.header__theme');
+const themeButtonSvg = document.querySelector('.header__theme use');
+const headerBackground = document.querySelector('.header .container');
+const heroBackground = document.querySelector('.hero .container');
+const contactsBackground = document.querySelector('.contacts .container');
+const buttons = document.querySelectorAll('.button');
+
 /*----------------------- hamburger button --------------------------*/
 const hamburgerButton = document.querySelector('.hamburger-button');
 const navMenu = document.querySelector('.header-nav');
@@ -12,6 +20,34 @@ navMenu.addEventListener('click', (event) => {
     if (event.target.className === 'header-nav__link')
         hamburgerToggle();
 });
+/*------------------------- theme change ----------------------------*/
+const changeTheme = (event) => {
+    if (theme === 0) {
+        document.body.classList.add('light-theme');
+        headerBackground.classList.add('light-theme');
+        heroBackground.classList.add('light-theme');
+        contactsBackground.classList.add('light-theme');
+        document.documentElement.style.setProperty('--text-color', '#000000');
+        buttons.forEach(button => { 
+            button.classList.add('light-theme');
+        });
+        themeButtonSvg.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', "assets/svg/icons.svg#dark-theme");
+        theme = 1;
+    } else {
+        document.body.classList.remove('light-theme');
+        headerBackground.classList.remove('light-theme');
+        heroBackground.classList.remove('light-theme');
+        contactsBackground.classList.remove('light-theme');
+        document.documentElement.style.setProperty('--text-color', '#ffffff');
+        buttons.forEach(button => { 
+            button.classList.remove('light-theme');
+        });
+        themeButtonSvg.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', "assets/svg/icons.svg#light-theme");
+        theme = 0;
+    }
+};
+
+themeButton.addEventListener('click', (event) => changeTheme(event));
 
 
 
